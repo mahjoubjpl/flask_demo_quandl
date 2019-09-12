2abe844181ef78944318c7bcd0344da70455b9f4
Ahmed Mahjoub <mahjoubjpl@gmail.com>
2019-09-12 08:47:38 -0700

    fi

--- app.py
+++ app.py
@@ -79,11 +79,12 @@ def index():
         if not tick.isalpha():
             return invalid()
         ticker_data = data_getter(tick)
-		
-		plot_setter(ticker_data, tick)
-		
-        
-        return plot_setter(ticker_data, tick)
+        '''
+		fig = plot_setter(ticker_data, tick)
+        '''
+        '''
+        script, div = components(fig)'''
+        return render_template('toolbar.html')
 
 if __name__ == '__main__':
   app.run(port=33507)

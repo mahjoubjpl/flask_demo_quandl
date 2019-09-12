7ac7445b2578a3095baf843e70c5cdc8cf249324
Ahmed Mahjoub <mahjoubjpl@gmail.com>
2019-09-12 08:23:39 -0700

    fi

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def plot_setter(df, ticker):
     p.outline_line_color = None
     p.yaxis.axis_label = "Closing price"
     p.xaxis.axis_label = "Date"
-    return show(p)
+    return p
 
 
 def error_message():

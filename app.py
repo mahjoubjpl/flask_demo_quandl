aa6219c9ed1eaa4e54d9895798dba27ead61d1a6
Ahmed Mahjoub <mahjoubjpl@gmail.com>
2019-09-11 21:40:58 -0700

    initial commit

--- app.py
+++ app.py
@@ -85,6 +85,6 @@ def index():
         return render_template(
             'newtab.html')
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    # app.run(debug=True)
+  app.run(port=33507)

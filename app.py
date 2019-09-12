91ba0aa8485283696fe48f9b90f43fd288037124
Ahmed Mahjoub <mahjoubjpl@gmail.com>
2019-09-12 15:13:44 -0700

    fi

--- app.py
+++ app.py
@@ -36,11 +36,13 @@ def plot_setter(df, ticker):
     hover.mode = 'vline'
     hover.line_policy = 'nearest'
     p.add_tools(hover)
-	
+    crosshair = CrosshairTool()
+    crosshair.dimensions = 'height'
+    crosshair.line_color = "#ffffff"
+    p.add_tools(crosshair)
 
-
-    datatoshow = ColumnDataSource(df)
-    p.line('date', 'close', source = datatoshow, color="#44ddaa")
+    dfcds = ColumnDataSource(df)
+    p.line('date', 'close', source = dfcds, color="#44ddaa")
 
     p.xaxis.formatter=DatetimeTickFormatter(days=["%d %b"])
     p.x_range=Range1d(df['date'].min(), df['date'].max())
@@ -49,16 +51,24 @@ def plot_setter(df, ticker):
     p.toolbar_location = None
 
     # Style plot
-    
+    p.background_fill_color = "#234567"
     p.border_fill_color = "#234567"
     p.title.text_color = "#ffffff"
     p.title.text_font_size = "1.25em"
     p.axis.major_label_text_color = "#ffffff"
     p.axis.major_label_text_font_size = "0.875em"
     p.axis.axis_line_color = "#ffffff"
+    p.axis.major_tick_line_color = "#ffffff"
+    p.axis.minor_tick_line_color = "#ffffff"
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_alpha = 0.5
+    p.ygrid.grid_line_dash = [4, 6]
     p.outline_line_color = None
     p.yaxis.axis_label = "Closing price"
-	p.background_fill_color = "green"
+    p.yaxis.axis_label_text_color = "#ffffff"
+    p.yaxis.axis_label_text_font_size = "1em"
+    p.yaxis.axis_label_text_font_style = "normal"
+    p.yaxis.axis_label_standoff = 12
     
     return p
 

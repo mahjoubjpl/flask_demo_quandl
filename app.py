104056ad7c8ab47b34c61bef7e5d0c253c88051f
Ahmed Mahjoub <mahjoubjpl@gmail.com>
2019-09-11 20:44:18 -0700

    initial commit

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ def index():
 def about():
   return render_template('about.html')
 
-if __name__ == '__main__':
-  app.run(port=33507)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)

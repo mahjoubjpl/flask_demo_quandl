2932b3cb87dad749893e0de9cf2ea6f9bf3c54d9
Ahmed Mahjoub <mahjoubjpl@gmail.com>
2019-09-12 14:29:29 -0700

    fi

--- app.py
+++ app.py
@@ -1,63 +1,98 @@
 import requests
 import pandas as pd
 from flask import Flask, render_template, request, redirect
-
-from bokeh.plotting import figure, show, output_file
-
+from datetime import date
+from dateutil.relativedelta import relativedelta
+from bokeh.plotting import figure, show, output_notebook
 from bokeh.models import ColumnDataSource, DatetimeTickFormatter, \
     Range1d, HoverTool, CrosshairTool
 from bokeh.embed import components
-import quandl
 
 app = Flask(__name__)
 
 apikey = "aV6ixxytw_ZyLnA5YZyT"
 
-def data_getter(ticker):
-    quandl.ApiConfig.api_key = 'aV6ixxytw_ZyLnA5YZyT'
-    data = quandl.get_table('WIKI/PRICES', ticker = [ticker], 
-                        qopts = { 'columns': ['date', 'close'] }, 
-                        date = { 'gte': '2015-08-09', 'lte': '2015-09-09' }, 
-                        paginate=True)
-    data['date'] = pd.to_datetime(data['date'])
+urlhead = "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker="
+urldate = "&date.gte="
+urlcols = "&qopts.columns="
+urltail = "&api_key=" + apikey
 
-    return data
+monthago = date.today() + relativedelta(months=-1)
+date = monthago.strftime("%Y%m%d")
 
+cols = "date,close"
 
-def plot_setter(df, ticker):
+def get_ticker(ticker):
+    """Retrieve and process Quandl data for given ticker symbol.
+    Retrieves the last month of closing price data for the given ticker
+    symbol. Returns a pandas DataFrame.
+    """
+    url = urlhead + ticker + urldate + date + urlcols + cols + urltail
+    try:
+        page = requests.get(url)
+        json = page.json()
+    except ValueError:
+        return pd.DataFrame()
+    df = pd.DataFrame(json['datatable']['data'], columns=['date','close'])
+    df['date'] = pd.to_datetime(df['date'])
+    df['date_str'] = df['date'].map(lambda x: x.strftime("%Y-%m-%d"))
+    df['close_str'] = df['close'].map(lambda x: '{:,.2f}'.format(x))
+    return df
+
+def bokehplot(df, ticker):
+    """Create a time-series line plot in Bokeh."""
+    p = figure(width=600, height=300, title=ticker.upper(), tools="")
+
+    hover = HoverTool(tooltips = """
+    <div>
+    <table>
+    <tr><td class="ttlab">Date:</td><td>@date_str</td></tr>
+    <tr><td class="ttlab">Close:</td><td>@close_str</td></tr>
+    </table>
+    </div>
+    """)
     
-    
-    p = figure(width=700, height=400, title="data ticker:"+ticker, tools="")
-
-    hover = HoverTool(tooltips=[
-        ( 'date',   '@date{%F}'            ),
-        ( 'close',  '$@close{%0.2f}' ),], formatters={
-        'date'      : 'datetime', 
-        'close' : 'printf'})
     hover.mode = 'vline'
     hover.line_policy = 'nearest'
     p.add_tools(hover)
+
     crosshair = CrosshairTool()
     crosshair.dimensions = 'height'
+    crosshair.line_color = "#ffffff"
     p.add_tools(crosshair)
 
-    data_to_show = ColumnDataSource(df)
-    p.line('date', 'close', source = data_to_show, color="#44ddaa")
+    dfcds = ColumnDataSource(df)
+    p.line('date', 'close', source = dfcds, color="#44ddaa")
 
     p.xaxis.formatter=DatetimeTickFormatter(days=["%d %b"])
     p.x_range=Range1d(df['date'].min(), df['date'].max())
 
-    p.title.text_font_size = "2em"
+    p.toolbar.logo = None
+    p.toolbar_location = None
+
+    # Style plot
+    p.background_fill_color = "#234567"
+    p.border_fill_color = "#234567"
+    p.title.text_color = "#ffffff"
+    p.title.text_font_size = "1.25em"
+    p.axis.major_label_text_color = "#ffffff"
     p.axis.major_label_text_font_size = "0.875em"
+    p.axis.axis_line_color = "#ffffff"
+    p.axis.major_tick_line_color = "#ffffff"
+    p.axis.minor_tick_line_color = "#ffffff"
     p.xgrid.grid_line_color = None
     p.ygrid.grid_line_alpha = 0.5
+    p.ygrid.grid_line_dash = [4, 6]
     p.outline_line_color = None
     p.yaxis.axis_label = "Closing price"
-    p.xaxis.axis_label = "Date"
+    p.yaxis.axis_label_text_color = "#ffffff"
+    p.yaxis.axis_label_text_font_size = "1em"
+    p.yaxis.axis_label_text_font_style = "normal"
+    p.yaxis.axis_label_standoff = 12
+    
     return p
 
-
-def error_message():
+def invalid():
     error = None
     with open("static/error.html") as err:
         error = err.read()
@@ -73,21 +108,22 @@ def main():
 @app.route('/index', methods=['GET','POST'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('index.html', bokeh_script="", bokeh_div="")
     else:
         tick = request.form['ticker_text']
         if not tick.isalpha():
             return invalid()
-        ticker_data = data_getter(tick)
-        
-		fig= plot_setter(ticker_data, tick)
-		script, div = components(fig)
-        
-        
+        ticker_df = get_ticker(tick)
+        if ticker_df.empty:
+            return invalid()
+        fig = bokehplot(ticker_df, tick)
+        script, div = components(fig)
         return render_template(
             'index.html',
             bokeh_script=script,
             bokeh_div=div)
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
+
+

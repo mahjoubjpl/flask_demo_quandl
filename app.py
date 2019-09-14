65a0c55ab9cc2119cae7ec305802c2f76f3ca29a
mahjoubjpl <48161502+mahjoubjpl@users.noreply.github.com>
2019-09-13 18:24:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import requests
 import pandas as pd
 from flask import Flask, render_template, request, redirect
 from datetime import date
-from dateutil.relativedelta import relativedelta
-from bokeh.plotting import figure, show, output_notebook
+from bokeh.plotting import figure, show
 from bokeh.models import ColumnDataSource, DatetimeTickFormatter, \
-    Range1d, HoverTool, CrosshairTool
+    Range1d, HoverTool
 from bokeh.embed import components
 import quandl
 
@@ -44,19 +43,16 @@ def plot_setter(df, ticker):
     p.xaxis.formatter=DatetimeTickFormatter(days=["%d %b"])
     p.x_range=Range1d(df['date'].min(), df['date'].max())
     p.toolbar.logo = None
-    p.toolbar_location = None
-
-   
+    p.toolbar_location = None  
     p.title.text_color = "#000000"
-    p.title.text_font_size = "1.25em"
+    p.title.text_font_size = "1.5em"
     p.axis.major_label_text_color = "#000000"
-    p.axis.major_label_text_font_size = "0.875em"
+    p.axis.major_label_text_font_size = "1.25em"
     p.xgrid.grid_line_color = None
     p.ygrid.grid_line_alpha = 0.5
-    p.ygrid.grid_line_dash = [4, 6]
+    p.ygrid.grid_line_dash = [2, 4]
     p.outline_line_color = None
-    p.yaxis.axis_label = "Closing price"
-    p.yaxis.axis_label_standoff = 12
+    p.yaxis.axis_label = "Close"
     
     return p
 
@@ -76,11 +72,9 @@ def main():
 @app.route('/index', methods=['GET','POST'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html', bokeh_script="", bokeh_div="")
+        return render_template('index.html', plot_script="", plot_div="")
     else:
         tick = request.form['ticker_text']
-        if not tick.isalpha():
-            return invalid()
         ticker_df = get_ticker(tick)
         if ticker_df.empty:
             return invalid()
@@ -88,8 +82,8 @@ def index():
         script, div = components(fig)
         return render_template(
             'index.html',
-            bokeh_script=script,
-            bokeh_div=div)
+            plot_script=script,
+            plot_div=div)
 
 if __name__ == '__main__':
     app.run(port=33507)
